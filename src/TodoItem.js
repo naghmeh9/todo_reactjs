79eee9aca11b809b58d3e58369441400f4e61b58
Naghmeh mohammadi <naghmehmohammadi@Naghmehs-MacBook-Air.local>
2020-04-28 21:11:50 +0800

    todo list with reactjs

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,21 @@
 import React from "react";
 
 function TodoItem(props) {
+  const compeletedStyle = {
+    fontStyle: "italic",
+    color: "#cdcdcd",
+    textDecoration: "line-through",
+  };
   return (
     <div className="todo-item">
-      <input type="checkbox" />
-      <p>{props.item.text}</p>
+      <input
+        type="checkbox"
+        checked={props.item.compeleted}
+        onChange={() => props.handleChange(props.item.id)}
+      />
+      <p style={props.item.completed ? compeletedStyle : null}>
+        {props.item.text}
+      </p>
     </div>
   );
 }
