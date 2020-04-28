19a53b49d974aabd5aa865aad4e4bf18cc94747c
Naghmeh mohammadi <naghmehmohammadi@Naghmehs-MacBook-Air.local>
2020-04-28 16:08:37 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+import todosData from "./todosData";
+import "./App.css";
+
+function App() {
+  const todoItems = todosData.map((item) => (
+    <TodoItem key={item.id} item={item} />
+  ));
+
+  return <div className="todo-list">{todoItems}</div>;
+}
+
+export default App;
